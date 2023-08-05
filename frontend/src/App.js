eed5796a275ce4ee8fcedf5809e159af54413905
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2023-08-05 17:08:31 +0530

    Login Page (errors assigned for wrong input)
    -errors has been assigned on the wrong input from the user
    -just need to align the error messages in a good pop-up
    -code has been optimized, but need a bit of touch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import TwofaForm from "./pages/Twofa";
+// import TwofaForm from "./pages/Twofa";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <TwofaForm />
+      <Login />
     </div>
   );
 }
--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,77 +1,220 @@
 import React from "react";
-import './Login.css'
-import { ReactComponent as LogoIcon } from './schwarz.svg';
-import { ReactComponent as GoogleIcon } from './google.svg';
-import { ReactComponent as TwitterIcon } from './twitter.svg';
+import "./Login.css";
+import { useState } from "react";
+import { ReactComponent as LogoIcon } from "./schwarz.svg";
+import { ReactComponent as GoogleIcon } from "./google.svg";
+import { ReactComponent as TwitterIcon } from "./twitter.svg";
 import { ReactComponent as AppleIcon } from "./apple.svg";
 
+const socialButtons = [
+  {
+    icon: <GoogleIcon className="google-icon" />,
+    text: "Continue with Google",
+    className: "google-btn",
+  },
+  {
+    icon: <AppleIcon className="apple-icon" />,
+    text: "Continue with Apple",
+    className: "apple-btn",
+  },
+  {
+    icon: <TwitterIcon className="twitter-icon" />,
+    text: "Continue with Twitter",
+    className: "twitter-btn",
+  },
+];
 
-const LoginForm = ()=>{
-    return(
-        
-          <div class="container">
-            <div className="log-form">
-              <LogoIcon className="logoIcon"/>
-              <p className="bold-text">Create an Account</p>
-              <p className="fade-text">Create your Schwarz ID and unlock a world of possibilities.</p>
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [showErrors, setShowErrors] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
 
-              <div className="btn-container">
-                <button className="active-btn">Sign in</button>
-                <button className="passive-btn">Sign up</button>
-              </div>
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
 
-              <div className="log-method">
-              <button className="google-btn"><span><GoogleIcon className="google-icon" /></span> <span>Continue with Google</span></button>
-              <button className="apple-btn"><span><AppleIcon className="apple-icon"/></span> <span>Continue with Apple</span></button>
-              <button className="twitter-btn"><span><TwitterIcon className="twitter-icon"/></span> <span>Continue with Twitter</span></button>
-              
-              {/* section of ---- or ---- */}
-              <div className="or-section"><div className="hr-line"></div><span>or</span><div className="hr-line"></div></div>
-              </div>
+    if (!newEmail.includes("@")) {
+      setEmailError('Email should contain "@" symbol');
+    } else {
+      setEmailError("");
+    }
+  };
 
-              <div className="name">
-                <div className="text-group">
-                  <label for="firstName">First Name</label>
-                  <input type="text" id="firstName" placeholder="Enter your first name" className=""/>
-                </div>
-                
-                <div className="text-group">
-                  <label for="lastName">Last Name</label>
-                  <input type="text" id="lastName" placeholder="Enter your last name" className=""/>
-                </div>
-              </div>
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
 
-              <div className="log-detail">
-              <label for="email">Email</label>
-              <input type="email" placeholder="Enter your email" className="email-inp"/>
+    if (newPassword.length < 8 || (newPassword.match(/\d/g) || []).length < 2) {
+      setPasswordError(
+        "Password should be at least 8 characters long and contain at least 2 numbers"
+      );
+    } else {
+      setPasswordError("");
+    }
+  };
+  const handleNextStepClick = () => {
+    // Set showErrors to true to display errors when the button is clicked
+    setShowErrors(true);
 
-              <label for="password">Password</label>
-              <input type="password" placeholder="Enter your password"/>
-              </div>
-             
-                {/* <div class="checkbox-container">
-                <div class="left">
-                  <input type="checkbox" class="check-btn" id="rememberMe" />
-                  <label for="rememberMe">Remember me</label>
-                </div>
-                <div class="right">
-                  <a href="#" class="forgot-password">Forgot password</a>
-                </div>
-              </div> */}
+    // Check for errors and show them if present
+    if (!email.includes("@")) {
+      setEmailError('Email should contain "@" symbol');
+    }
 
-                 {/* <input type="submit" /> */}
-                 {/* <button className="next">Next Step</button> */}
-                <a href="" className="next">Next Step</a>
+    if (password.length < 8 || (password.match(/\d/g) || []).length < 2) {
+      setPasswordError(
+        "Password should be at least 8 characters long and contain at least 2 numbers"
+      );
+    }
 
-            </div>
-            <div className="bg-vid">
-              {/* Looping Video of Simulation from Blender */}
-              <video autoplay loop muted>
-                <source src="https://media.istockphoto.com/id/1251524036/video/morphing-holographic-liquid-blobs-abstract-3d-animation.mp4?s=mp4-640x640-is&k=20&c=c8oAYJoLD-_B_xjmfGGlss3j7PRi9i5KKpmaVbyy71A=.mp4" type="video/mp4"/>
-              </video>
-              </div>            
+    if (!email.includes("@")) {
+      setEmailError('Email should contain "@" symbol');
+    }
+
+    if (password.length < 8 || (password.match(/\d/g) || []).length < 2) {
+      setPasswordError(
+        "Password should be at least 8 characters long and contain at least 2 numbers"
+      );
+    }
+
+    if (firstName.trim() === "") {
+      setFirstNameError("Please enter your first name");
+    }
+
+    if (lastName.trim() === "") {
+      setLastNameError("Please enter your last name");
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="log-form">
+        <LogoIcon className="logoIcon" />
+        <p className="bold-text">Create an Account</p>
+        <p className="fade-text">
+          Create your Schwarz ID and unlock a world of possibilities.
+        </p>
+
+        <div className="btn-container">
+          <button className="active-btn">Sign in</button>
+          <button className="passive-btn">Sign up</button>
+        </div>
+
+        <div className="log-method">
+          {socialButtons.map((button, index) => (
+            <button key={index} className={button.className}>
+              <span>{button.icon}</span> <span>{button.text}</span>
+            </button>
+          ))}
+
+          {/* section of ---- or ---- */}
+          <div className="or-section">
+            <div className="hr-line"></div>
+            <span>or</span>
+            <div className="hr-line"></div>
           </div>
-       
-    );
-}
+        </div>
+
+        <div className="name">
+          <div className="text-group">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              placeholder="Enter your first name"
+              className={`${
+                firstNameError && showErrors ? "error-border" : ""
+              }`}
+              value={firstName}
+              onChange={(event) => {
+                const newName = event.target.value;
+                setFirstName(newName);
+
+                if (newName.trim() === "") {
+                  setFirstNameError("Please enter your first name");
+                } else {
+                  setFirstNameError("");
+                }
+              }}
+            />
+            {firstNameError && showErrors && (
+              <p className="error">{firstNameError}</p>
+            )}
+          </div>
+
+          <div className="text-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              placeholder="Enter your last name"
+              className={`${lastNameError && showErrors ? "error-border" : ""}`}
+              value={lastName}
+              onChange={(event) => {
+                const newName = event.target.value;
+                setLastName(newName);
+
+                if (newName.trim() === "") {
+                  setLastNameError("Please enter your last name");
+                } else {
+                  setLastNameError("");
+                }
+              }}
+            />
+            {lastNameError && showErrors && (
+              <p className="error">{lastNameError}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="log-detail">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className={`email-inp ${
+              emailError && showErrors ? "error-border" : ""
+            }`}
+            value={email}
+            onChange={handleEmailChange}
+          />
+          {emailError && showErrors && <p className="error">{emailError}</p>}
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className={`password-inp ${
+              passwordError && showErrors ? "error-border" : ""
+            }`}
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          {passwordError && showErrors && (
+            <p className="error">{passwordError}</p>
+          )}
+        </div>
+
+        <a href="#" className="next" onClick={handleNextStepClick}>
+          Next Step
+        </a>
+      </div>
+      <div className="bg-vid">
+        {/* Looping Video of Simulation from Blender */}
+        <video autoPlay loop muted>
+          <source
+            src="https://media.istockphoto.com/id/1251524036/video/morphing-holographic-liquid-blobs-abstract-3d-animation.mp4?s=mp4-640x640-is&k=20&c=c8oAYJoLD-_B_xjmfGGlss3j7PRi9i5KKpmaVbyy71A=.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+    </div>
+  );
+};
 export default LoginForm;
